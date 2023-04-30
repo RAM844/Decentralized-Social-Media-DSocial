ce34b3a5c33d4928242f5469a59e52f948bcb6dd
Shrira Balaji <balajikarnam844@gmail.com>
2023-04-30 15:11:06 +0530

    generating challenge and sigin the challenge and sending signed challenge to the lens api is done

--- src/lib/auth/useLogin.ts
+++ src/lib/auth/useLogin.ts
@@ -1,14 +1,44 @@
+import { useAddress, useSDK } from "@thirdweb-dev/react";
+import generateChallenge from "./generateChallenge";
+import { useAuthenticateMutation } from "@/graphql/generated";
+import { useMutation } from "@tanstack/react-query";
 // 0. use has a connected wallet 
-// 1.generate a challenge which comes from lens api
-// 2. Sign the challenge from the user's wallet
-// 3. send the signed challenge to the lens api
-// 4. receive a access token from the Lens api if we succed 
-// 5. Store the access token inside local storage so we can use it
+
+
+
+
+
 
 export default function useLogin(){
-    // 1.write the actual async function 
+    const address = useAddress();
+    const sdk = useSDK();
 
+    const {mutateAsync:sendSignedMessage } = useAuthenticateMutation();
+// 1.write the actual async function 
+    async function login() {
+        if(!address)return;
+        // 1.generate a challenge which comes from lens api
+        const { challenge } = await generateChallenge(address);
+
+       // 2. Sign the challenge from the user's wallet
+       const signature = await sdk?.wallet.sign(challenge.text);
+
+       // 3. send the signed challenge to the lens api
+       const { authenticate } = await sendSignedMessage({
+        request:{
+            address:address,
+            signature:signature,
+        }
+       })
+       console.log("Authencated",authenticate);
+       
+       // 4. receive a access token from the Lens api if we succed 
+
+       // 5. Store the access token inside local storage so we can use it
+
+    }
 
     //2.return useMutation hook from wrapping the asynch function 
+    return useMutation(login);
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,14 @@ import {
   PublicationSortCriteria,
   useExplorePublicationsQuery
 } from "@/graphql/generated"
+import useLogin from "@/lib/auth/useLogin";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react"
 
 export default function Home() {
-  const { data, isLoading, error } = useExplorePublicationsQuery(
-    {
-      request: {
-        sortCriteria: PublicationSortCriteria.TopMirrored
-      }
-    }
-  );
-
-  console.log({
-    data,
-    isLoading,
-    error
-  });
-
-  return (
-    <>hello world</>
-  )
+  const address = useAddress();
+  const {mutate:requestLogin} = useLogin();
+  if(!address){
+    return(<ConnectWallet/>)
+  }
+  return <button onClick={()=>requestLogin()}>Login</button>
 }
